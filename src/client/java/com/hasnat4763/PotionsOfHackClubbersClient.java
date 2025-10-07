59ee003628e9f4df5355789ba916eccc49911e44
Hasnat4763 <abulhasnat5737@gmail.com>
2025-10-07 22:27:53 +0600

    Initial commit

---
+++ src/client/java/com/hasnat4763/PotionsOfHackClubbersClient.java
@@ -0,0 +1,10 @@
+package com.hasnat4763;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PotionsOfHackClubbersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/hasnat4763/item/ModItems.java
@@ -0,0 +1,16 @@
+package com.hasnat4763.item;
+
+
+import com.hasnat4763.PotionsOfHackClubbers;
+
+import static com.hasnat4763.PotionsOfHackClubbers.MOD_ID;
+
+
+public class ModItems {
+
+
+
+    public static void RegisterModItems(){
+        PotionsOfHackClubbers.LOGGER.info("Registering items for " + MOD_ID);
+    }
+}
